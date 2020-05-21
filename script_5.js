a340eba826a57c347aa37b0d9c78c31c6f31116e
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-21 13:50:10 +0200

    finished scripts from file 5

--- script_5.js
+++ script_5.js
@@ -39,9 +39,67 @@ isRented(books);
 
 
 //Quel est livre le plus emprunté ?
+function mostRead(books){
+  let mostRead = "";
+  let k = 0;
+  console.log("Le livre le plus emprunté :");
+  books.forEach(book => {
+      if (book.rented > k) {
+          k = book.rented;
+          mostRead = book.title;
+      }
+    });
+  console.log(mostRead);
 
+}
+
+mostRead(books);
 
 //Quel est le livre le moins emprunté ?
+
+function lessRented(books) {
+  console.log("Le livre le moins emprunté :");
+  let booksRentedArray = books.sort(function (a, b) {
+    return a.rented - b.rented;
+  });
+
+  let textToReturn = booksRentedArray[0].title;
+  console.log(textToReturn);
+}
+
+lessRented(books);
+
 //Trouve le livre avec l'ID: 873495 ;
-//Supprime le livre avec l'ID: 133712 ;
-//Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé).
+
+function findBook(books){
+  let findBook = books.find(item => item.id == 873495);
+  console.log("Le livre avec l'ID: 873495 est : " + findBook.title);
+
+}
+
+findBook(books)
+
+//Supprime le livre avec l'ID: 133712 
+function deleteBook(books){
+  
+  let bookDelete = books.findIndex(item => item.id === 133712);
+  console.log("Le livre avec l'ID 133712 a été suprimé" );
+  books.splice(bookDelete,1);
+  console.log(books);
+  
+}
+
+deleteBook(books);
+
+//Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé)
+function sortBook(books) {
+  console.log("Voici la liste para ordre alphabétique");
+  let booksNameArray = [];
+
+  books.forEach(item =>{
+    booksNameArray.push(item.title);
+  });
+  console.log(booksNameArray.sort());
+}
+
+sortBook(books)
